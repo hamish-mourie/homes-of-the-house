38ae2e89880a080b8d1478d3ad22a2ce88a45fde
Hamish Mourie <20253849+hamish-mourie@users.noreply.github.com>
2022-09-11 15:24:57 +1200

    Add sortable bar graph

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,14 +3,48 @@ import { getMPs } from '../apiClient'
 
 import AddMP from './AddMP'
 import MPTable from './MPTable'
+import BarChart from './BarChart'
 
 function App() {
   const [MPs, setMPs] = useState([])
+  const [chartData, setChartData] = useState('')
 
   function reloadMPs(sortBy) {
     getMPs(sortBy)
       .then((result) => {
         setMPs(result)
+        let backgroundColorData = []
+        result.forEach((element) => {
+          if (element.politicalParty === 'National') {
+            backgroundColorData.push('blue')
+          } else if (element.politicalParty === 'Labour') {
+            backgroundColorData.push('red')
+          } else if (element.politicalParty === 'Green') {
+            backgroundColorData.push('green')
+          } else if (element.politicalParty === 'Māori') {
+            backgroundColorData.push('black')
+          } else if (element.politicalParty === 'ACT') {
+            backgroundColorData.push('yellow')
+          } else {
+            backgroundColorData.push('gray')
+          }
+        })
+        setChartData({
+          labels: result.map((element) => {
+            return element.fullName
+          }),
+          datasets: [
+            {
+              label: 'Number of Properties Owned',
+              data: result.map((element) => {
+                return element.totalPropertiesOwned
+              }),
+              backgroundColor: backgroundColorData,
+              borderColor: 'black',
+              borderWidth: 2,
+            },
+          ],
+        })
       })
       .catch((err) => {
         console.error(err.message)
@@ -21,6 +55,38 @@ function App() {
     getMPs('fullName')
       .then((result) => {
         setMPs(result)
+        let backgroundColorData = []
+        result.forEach((element) => {
+          if (element.politicalParty === 'National') {
+            backgroundColorData.push('blue')
+          } else if (element.politicalParty === 'Labour') {
+            backgroundColorData.push('red')
+          } else if (element.politicalParty === 'Green') {
+            backgroundColorData.push('green')
+          } else if (element.politicalParty === 'Māori') {
+            backgroundColorData.push('black')
+          } else if (element.politicalParty === 'ACT') {
+            backgroundColorData.push('yellow')
+          } else {
+            backgroundColorData.push('gray')
+          }
+        })
+        setChartData({
+          labels: result.map((element) => {
+            return element.fullName
+          }),
+          datasets: [
+            {
+              label: 'Number of Properties Owned',
+              data: result.map((element) => {
+                return element.totalPropertiesOwned
+              }),
+              backgroundColor: backgroundColorData,
+              borderColor: 'black',
+              borderWidth: 2,
+            },
+          ],
+        })
       })
       .catch((err) => {
         console.error(err.message)
@@ -30,6 +96,13 @@ function App() {
   return (
     <div>
       <h1 className="center">Homes of the House</h1>
+      {chartData !== '' && (
+        <div className="center graph">
+          {console.log(chartData)}
+          <BarChart chartData={chartData} />
+        </div>
+      )}
+
       <MPTable props={{ reloadMPs, MPs }} />
       <AddMP reloadMPs={reloadMPs} />
     </div>
